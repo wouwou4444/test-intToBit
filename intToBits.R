3f7927f0a3d18bfb8afff58cd3466c6d7168f56c
wouwou4444 <valery.buz@outlook.com>
2018-10-04 02:31:57 +0200

    Add separator to drop and remove trailing separator in words

--- intToBits.R
+++ intToBits.R
@@ -1,7 +1,10 @@
 separator <- c("_",".", "-")
-text_sample <- c("serveur_ADBCRD01,nom1-nom2-nom3. test de differents sep, EM:listener_ORAADB01_dg_DG01_12654","Autre fichier avec des adresses IP 123.34.213.2 mais pas eulement. et inum ut corpus.")
+text_sample <- c("serveur_ADBCRD01,nom1-nom2-nom3. test de differents sep, EM:listener_ORAADB01_dg_DG01_12654",
+                 "Bonjour autre FICHIER, demain, est Autre fichier avec des adresses IP 123.34.213.2 mais pas eulement. et inum ut corpus.",
+                 "Encore un autre exemple, example, 12.153.123.54 et dg_vg87_spqs_psf")
 
-
+separator_toremove <- "[,\t :]"
+text_sample <- (gsub(pattern = separator_toremove, replacement = " ",x = text_sample))
 #### transform string based on different separators
 for (j in 1:(i-1)) {
   # print(j)
@@ -23,5 +26,7 @@ list_description_tokenizations <- sapply(1:(i-1), function(j) {
 
 #### concatenate the different tokens from each regexp transformation 
 #### into on single list of unique tokens
-list_tokens <- apply(list_description_tokenizations, 1, FUN = function (x) { unique(unlist(c(x)))})
-
+gexpr <- "[_._,:]$"
+list_tokens <- apply(list_description_tokenizations, 1, 
+                     FUN = function (x) { unique(gsub(pattern = gexpr,replacement = "",tolower(unlist(c(x)))))})
+str((list_tokens))
